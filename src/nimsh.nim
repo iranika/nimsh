3cea0a5c30bfa45ebcf28700539f72b86adfced0
iranika <7370486+iranika@users.noreply.github.com>
2019-05-30 21:56:43 +0900

    add: ビルトインコマンドへの参照を実装

--- src/nimsh.nim
+++ src/nimsh.nim
@@ -3,26 +3,63 @@
 
 import os, osproc, rdstdin, strformat, strutils, terminal, times, strformat
 
+
+proc lsh_read_line(): string =
+  result = readLineFromStdin(">").strip
+
+
+const LSH_TOK_SEPS: set[char] = {' ','\t','\r','\n','\a'}
+proc lsh_split_line(line: string): seq[string] =
+  result = line.split(LSH_TOK_SEPS)
+
+proc lsh_cd(args: seq[string]): int =
+  echo "cd is builtin command."
+  echo "args :" & $args
+  return 1
+
+proc lsh_ls(args: seq[string]): int =
+  echo "ls is builtin command."
+  echo "args :" & $args
+  return 1
+
+proc lsh_exit(args: seq[string]): int =
+  echo "Good bye"
+  return 0
+
+type builtin_command = object
+  name: string
+  fn: proc (args: seq[string]): int
 var
-  currentExpression = ""
+  BUILTIN_COMMANDS: seq[builtin_command] = @[
+    builtin_command(name:"ls", fn:lsh_ls),
+    builtin_command(name:"cd", fn:lsh_cd),
+    builtin_command(name:"exit", fn:lsh_exit),
+  ]
 
-proc getPromptSymbol(): string =
-  result = "nimsh> "
-
-proc cleanExit(exitCode = 0) =
-  quit(exitCode)
+proc lsh_execute(args: seq[string]): int =
+  if args.len == 0:
+    return 0
+  
+  for cmd in BUILTIN_COMMANDS:
+    if args[0] == cmd.name:
+      return cmd.fn(args)
 
 proc runForever() =
-  while true:
-    try:
-      currentExpression = readLineFromStdin(getPromptSymbol()).strip
-    except IOError:
-      continue
-    
-    if currentExpression in ["exit", "exit()", "quit", "quit()"]:
-      cleanExit()
-    
-    stdout.writeLine($currentExpression)
+  var line: string
+  var args: seq[string]
+  var status: int
+
+  while (status == 1):
+    line = lsh_read_line()
+    args = lsh_split_line(line)
+    status = lsh_execute(args)
+
+
+
+
+
+
+
 
 proc init() = 
   return
