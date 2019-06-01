50dc1e78398faec3cf8cbcad0374911c6cfb36bc
iranika <7370486+iranika@users.noreply.github.com>
2019-06-01 10:15:50 +0900

    add: 外部コマンドの呼び出しの実装

--- src/nimsh.nim
+++ src/nimsh.nim
@@ -1,12 +1,11 @@
 # This is just an example to get you started. A typical hybrid package
 # uses this file as the main entry point of the application.
 
-import os, osproc, rdstdin, strformat, strutils, terminal, times, strformat
+import rdstdin, strutils, terminal
 import posix
 
 proc lsh_read_line(): string =
   result = readLineFromStdin(">").strip
-
 
 const LSH_TOK_SEPS: set[char] = {' ','\t','\r','\n','\a'}
 proc lsh_split_line(line: string): seq[string] =
@@ -20,11 +19,6 @@ proc lsh_cd(args: seq[string]): int =
       stderr.write("lsh: argument is invalid.\n")
   return 0
 
-proc lsh_ls(args: seq[string]): int =
-  echo "ls is builtin command."
-  echo "args :" & $args
-  return 0
-
 proc lsh_exit(args: seq[string]): int =
   echo "Good bye"
   return -1
@@ -34,18 +28,39 @@ type builtin_command = object
   fn: proc (args: seq[string]): int
 var
   BUILTIN_COMMANDS: seq[builtin_command] = @[
-    builtin_command(name:"ls", fn:lsh_ls),
     builtin_command(name:"cd", fn:lsh_cd),
     builtin_command(name:"exit", fn:lsh_exit),
+    builtin_command(name:"quit", fn:lsh_exit),
   ]
 
+proc lsh_launch(args: seq[string]): int =
+  var status :cint
+
+  var pid = fork()
+  if pid == 0:
+    #child process
+    if execvp(args[0], args.allocCStringArray()) == -1:
+      stderr.write("command execute error.\n")
+    quit(0)
+  elif pid < 0:
+    stderr.write("fork error.\n")
+  else:
+    #Parent process  
+    let wpid = waitpid(pid, status, WUNTRACED)
+    if WIFEXITED(status):
+      return 0
+  return 0
+
 proc lsh_execute(args: seq[string]): int =
-  if args.len == 0:
-    return 0
+  if args.len == 1:
+    if isNilOrEmpty(args[0]):
+      return 0
   
   for cmd in BUILTIN_COMMANDS:
     if args[0] == cmd.name:
       return cmd.fn(args)
+  
+  return lsh_launch(args)
 
 proc runForever() =
   var line: string
@@ -57,15 +72,13 @@ proc runForever() =
     args = lsh_split_line(line)
     status = lsh_execute(args)
 
-
 proc init() = 
+  stdout.writeLine("Wellcome to nimsh! 🍣")
   return
 
 proc main() =
   init()
-
   runForever()
-
 
 when isMainModule:
   import cligen
